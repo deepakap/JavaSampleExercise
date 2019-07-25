d42f1ba81917a9c22084c760612960e2bfa6df91
deepak <deepak_ap007@yahoo.co.in>
2019-07-25 15:41:50 -0700

    added getters and setters for Student class
    Signed-off-by: deepak <deepak_ap007@yahoo.co.in>

--- src/com/algos/Student.java
+++ src/com/algos/Student.java
@@ -17,6 +17,30 @@ this.name=name;
 this.age=age;  
 }  
   
+public int getRollno() {
+	return rollno;
+}
+
+public void setRollno(int rollno) {
+	this.rollno = rollno;
+}
+
+public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+
+public int getAge() {
+	return age;
+}
+
+public void setAge(int age) {
+	this.age = age;
+}
+
 public int compareTo(Student st){  
 if(this.age==st.age)  
 return 0;  
